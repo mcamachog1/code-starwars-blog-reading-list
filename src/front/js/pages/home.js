f87b94a97000f080d597dd6c43290014c645ae18
Maryvi <mcamachog@hotmail.com>
2022-11-07 11:53:56 +0000

    varias tarjetas con titulo

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
 import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import { MyCard } from "../component/mycard";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import Example from "../component/example";
@@ -12,11 +12,13 @@ export const Home = () => {
 
   const getPeople = async () => {
     try {
+      //"https://www.swapi.tech/api/people";
       let url = "https://www.swapi.tech/api/people";
       let response = await fetch(url);
-      let data = await response.json();
-      setPeople(data);
-      console.log(data);
+      if (response.ok) {
+        let data = await response.json();
+        setPeople(data.results);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -28,25 +30,11 @@ export const Home = () => {
   }, []);
 
   return (
-    <Card style={{ width: "18rem" }}>
-      <div>
-        {console.log(JSON.stringify(people, null, 2))}
-        {/* {console.log(Object.values(people).map(e))};
-        {Object.values(people).map((i) =>
-          <p key={i.uid}>{i.name}</p>
-          console.log(i)
-        )} */}
-      </div>
-
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <div>
+      {console.log(JSON.stringify(people, null, 2))}
+      {people.map((item, index) => {
+        return <MyCard item={item} key={index} />;
+      })}
+    </div>
   );
 };
